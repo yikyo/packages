8e69decd4d4c785bd6e5741f8b0f60d7cb8094be
yiKyo <yikyo666@gmail.com>
2022-10-07 23:01:43 +0800

    feat: 添加 eslint-config-standard 库

---
+++ .changeset/hot-points-tan.md
@@ -0,0 +1,5 @@
+---
+'@yikyo/eslint-config-standard': major
+---
+
+添加 eslint-config-standard 库
---
+++ src/eslint-config-standard/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'import',
+    'simple-import-sort',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
